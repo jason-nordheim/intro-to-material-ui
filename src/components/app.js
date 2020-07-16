bddf3a4a294f4f274833e8fc0449f1b5c0ef653b
jason-nordheim <jason.nordheim@gmail.com>
2020-07-16 15:37:25 -0600

    scaffolded out application

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react' 
 import { Header, Footer } from './layouts' 
+import Exercises from './exercises'
 
 export default props => { 
   return (
     <>
       <Header />
+      <Exercises /> 
       <Footer /> 
     </>
   )
