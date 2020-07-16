ce6149fc4f52aadc698c6dce1f94c73650ed2315
jason-nordheim <jason.nordheim@gmail.com>
2020-07-16 15:56:20 -0600

    finished application scaffold

--- src/components/exercises/index.js
+++ src/components/exercises/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Grid } from '@material-ui/core'
+import LeftPane from './leftPane'
+import RightPane from './rightPane'
 
+const style = {
+  paper: { padding: 20, marginTop: 10, marginBottom: 10 },
+};
 
 export default props => {
   return (
-    <div> Hello from exercises </div>
+    <Grid container>
+      <Grid item sm>
+       <LeftPane styles={ style } /> 
+      </Grid>
+      <Grid item sm>
+        <RightPane styles={ style } /> 
+      </Grid>
+    </Grid>
   )
 }
--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
 
 export default (props) => {
   return (
-    <header>Header</header>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5" color="inherit">Exercise Database</Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
